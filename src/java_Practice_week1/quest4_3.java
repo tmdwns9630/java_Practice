8865d3a1d9f39763aec9cdd136069a4002cbd074
CSJ <tmdwns9630@gmail.com>
2022-12-10 21:54:33 +0900

    1주차 4,5번 수정 풀이 완료, 이메일 전송 완료.

--- src/java_Practice_week1/quest4_3.java
+++ src/java_Practice_week1/quest4_3.java
@@ -1,51 +1,32 @@
 package java_Practice_week1;
-
 import java.util.Scanner;
 
 /*
-//50명의 학생의 점수를 입력하고, 평균을 계산하는 프로그램을 작성하세요.
+//Q. 50명의 학생의 점수를 입력하고, 평균을 계산하는 프로그램을 작성하세요.
 
-1주차 연습습문제 4번에 대한 회신에서 “배열을 사용하지 않고, 전체 점수를 구한 후 평균을 낸다.”는 것은 평균을 계산하는 데
-
+피드백 : 1주차 연습습문제 4번에 대한 회신에서 “배열을 사용하지 않고, 전체 점수를 구한 후 평균을 낸다.”는 것은 평균을 계산하는 데
 있어서 굳이 배열에 점수를 저장하지 말고, 문제에서 원하는 평균값을 출력할 수 있으며, 그 방향으로 프로그램을 수정해 보라고
-
 회신한 것이었습니다(굳이 그렇게 회신한 이유는 문제에서 원하는 해답을 얻기 위한 최소한의 코드를 작성했으면 하는 의도라도
-
 이해해 주면 좋겠습니다.).
 */
 
+//Score 변수 하나에 연속해서 점수를 입력 받고, 이를 totalScore에 바로 누적되게 저장하고, 입력이 끝나면 double형으로 형변환하여 평균값 출력.
 public class quest4_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] Score = new int[50];
+		int Score;
 		int totalScore = 0;
+		int limit = 10;
 		
 		//50명의 점수를 입력.
 		try (Scanner sc = new Scanner(System.in)) {
-			for(int i=0; i<50; i++) {
+			for(int i=0; i<limit; i++) {
 				System.out.printf("%d번째 값 : ", i);
-				Score[i] = sc.nextInt();
+				Score = sc.nextInt();
+				totalScore += Score;
 			}
+			System.out.println("평균 점수 : " + (double)totalScore/limit);
 		}
-		
-		//입력한 점수를 출력.
-		for(int i=0; i<50; i++) {
-			System.out.print(i + " : " +Score[i]+" | ");
-			
-			// i가 10의 배수일 때마다 개행 문자 출력.
-			if(((i+1) % 10) == 0) {
-				System.out.print("\n");
-			}
-		}
-		
-		//전체 점수를 합하여 평균 점수 계산.
-		for(int i=0; i<50; i++) {
-			totalScore += Score[i];
-		}
-		System.out.println("평균 점수 : " + totalScore/50);
-
 	}
-	
-	
 }
--- src/java_Practice_week1/quest5.java
+++ src/java_Practice_week1/quest5.java
@@ -3,39 +3,41 @@ package java_Practice_week1;
 import java.util.Arrays;
 import java.util.Scanner;
 
-//5명의 학생의 이름을 입력 받아서 알파벳 순으로 정렬하는 프로그램을 작성하세요.
+//Q. 5명의 학생의 이름을 입력 받아서 알파벳 순으로 정렬하는 프로그램을 작성하세요.
 
 //피드백 : “Arrays.sort” 메소드를 사용하지 말고, 직접 배열을 정렬하여 결과를 출력할 수 있도록 프로그램을 수정해 주세요.
 public class quest5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] stuName = new String[5];
-//		
-//		try(Scanner sc = new Scanner(System.in)) {
-//			for(int i =0;i<5;i++) {
-//				System.out.print("학생"+i+": ");
-//				stuName[i] = sc.nextLine();
-//			}//for
-//		}//try
-//		
 		
-		stuName[0] = "Bark";
-		stuName[1] = "Genarte";
-		stuName[2] = "Harry";
-		stuName[3] = "Aniee";
-		stuName[4] = "Fourze";
+		try(Scanner sc = new Scanner(System.in)) {
+			for(int i =0;i<5;i++) {
+				System.out.print("학생"+i+": ");
+				stuName[i] = sc.nextLine();
+			}//for
+		}//try
 		
-//		예상 답안 : Aniee Bark Fourze Genarte Harry 
 		
+//		stuName[0] = "Bark";
+//		stuName[1] = "Denarte";
+//		stuName[2] = "Harry";
+//		stuName[3] = "Aniee";
+//		stuName[4] = "Fourze";
+		
+		//예상 답안 : Aniee Bark Genarte Fourze  Harry 
 		
 		//정렬 전
 		System.out.print("정렬 전 : ");
 		for(int i = 0; i< stuName.length; i++) {
 			System.out.print(stuName[i] + " ");
 		}//for
+		System.out.println();
 		
-		//Arrays 클래스의 sort 메서드를 사용하여 배열 정렬.
-		Arrays.sort(stuName);
+		
+	
+		//Arrays.sort(stuName);//Arrays 클래스의 sort 메서드를 사용하여 배열 정렬.
+		arrSort(stuName);
 		
 		//정렬 후
 		System.out.print("\n정렬 후 : ");
@@ -46,10 +48,19 @@ public class quest5 {
 	
 	
 	//compareTo() 를 사용하여 정렬해보자.
-	public void arrSort(String[] str) {
+	//기준값.compareTo(비교값)
+	//기준 > 비교값 : 양수(아스키코드 차이)
+	//기준 < 비교 : 음수 ( 아스키 차이)
+	
+	public static void arrSort(String[] str) {
+		String temp;
 		for(int i = 0; i< str.length-1; i++) {
-			for(int j = 0; j<str.length-1-i; j++) {
-				
+			for(int j = i+1; j<str.length; j++) {
+				if(str[i].compareTo(str[j])>0) {
+					temp = str[i];
+					str[i]=str[j];
+					str[j]=temp;
+				}
 			}
 		}
 	}
